4b14069c5499e0c93887af8fe05cdf95bd4c1fb1
Dmitriy Fetyukhin <dimaparachute11@icloud.com>
2022-06-10 23:32:50 +0300

    Completed algorithm

--- binarySearch/main.swift
+++ binarySearch/main.swift
@@ -5,7 +5,29 @@
 //  Created by Дмитрий Фетюхин on 10.06.2022.
 //
 
-import Foundation
+/// binary search algorithm based on Grokking Algorithms by Aditya Y. Bhargava
+func binarySearch(list: [Int], item: Int) -> Int? {
+    var low = 0
+    var high = list.count - 1
+    
+    while low <= high {
+        let mid = (low + high)/2
+        let guess = list[mid]
+        if guess == item {
+            return mid
+        }
+        if guess > item {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+    return nil
+}
 
-print("Hello, World!")
+let myList = [1, 3, 5, 7, 9]
+
+// if nil -> -1
+print(binarySearch(list: myList, item: 3) ?? -1)
+print(binarySearch(list: myList, item: -1) ?? -1)
 
